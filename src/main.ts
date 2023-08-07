53cd27dd912268658d339fd18392e0534e04328d
M-Raafay <m.rafay28749@gmail.com>
2023-08-07 14:57:25 +0500

    cors-middleware

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
+
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cors());
+  //app.enableCors();
   const configService = app.get(ConfigService)
 
   app.useGlobalPipes(
