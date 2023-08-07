26229ac484bd2f031a8ce61b19b7e243aa6ed945
M-Raafay <m.rafay28749@gmail.com>
2023-08-07 13:12:19 +0500

    implements cors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { INestApplication } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
-//  const configService = app.get(ConfigService)
-const swaggerConfig = new DocumentBuilder()
-    .setTitle('API with NestJS')
-    .setDescription('API developed throughout the API with NestJS course')
-    .setVersion('1.0')
-    .build();
- 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  app.enableCors();
+  const configService = app.get(ConfigService)
+
+  app.useGlobalPipes(
+    new ValidationPipe({whitelist:true}),
+  );
+  await app.listen(Number(configService.get('PORT')) || 3001);  
 }
 bootstrap();
--- src/members/dto/create-member.dto.ts
+++ src/members/dto/create-member.dto.ts
@@ -1,16 +1,29 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateMemberDto {
+    @IsString()
+    @IsNotEmpty()
     member_id :string
 
+
+    @IsString()
+    @MinLength(3)
+    @IsNotEmpty()
     username :string
 
+    @IsString()
     first_name: string;
-
+    
+    @IsString()
     last_name: string;
 
+    @IsEmail()
     email: string;
 
+    @IsString()
     TS: string;
 
+    @IsString()
     TL: string;
 
     Projects : string[]
